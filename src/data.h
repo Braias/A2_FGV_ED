73fa31545c4b3911c6198917e244797510720523
Sofia <sofia.moumont@gmail.com>
2025-06-02 20:08:12 -0300

    Documentando funções read_directory, load_doc e read_doc

--- src/data.h
+++ src/data.h
@@ -23,8 +23,28 @@ namespace DataHandling{
         vector<vector<int>> document_ids;
     };
 
+    /**
+    * @brief Lê os arquivos do diretório providenciado
+    * @param dir_path caminho para o diretório
+    * @return Vetor de caminhos para os documentos do diretório
+    */
     vector<string> read_directory(const string &dir_path);
+
+    /**
+    * @brief Carrega o documento de acordo com o path providenciado
+    * @param doc_path caminho para o documento
+    * @return String do conteúdo do arquivo
+    */
     string load_doc(const string &doc_path);
+
+    /**
+    * @brief Lê o conteúdo do doc e gera a estrutura Document, com sua id,
+    * seu nome baseado no path e um vetor de strings únicas a partir da string conteúdo
+    * @param doc_path caminho para o documento
+    * @param document_id id do documento
+    * @param conteudo conteúdo do documento
+    * @return Estrutura Document contendo um vetor de palavras únicas, nome e ID
+    */
     Document read_doc(const string &doc_path, int document_id);
 
     vector<WordAppearance> process_doc(const Document &doc);
