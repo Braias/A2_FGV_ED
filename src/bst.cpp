2b700c46ac3feab52f02e407577bf939182b9b5f
viniciodeusdara <viniciodeusdara@gmail.com>
2025-06-03 17:36:26 -0300

    feat: adicionei funcao search para bst

--- src/bst.cpp
+++ src/bst.cpp
@@ -1,9 +1,12 @@
 #include "tree_utils.h"
 #include "bst.h"
 #include <iostream>
+#include <chrono>
 
 // TODO: Adicionar insert
-// TODO: Adicionar search
+
+using namespace std;
+using namespace chrono;
 
 namespace BST {
 
@@ -19,7 +22,29 @@ InsertResult insert(BinaryTree* tree, const std::string& word, int documentId) {
 };
 
 SearchResult search(BinaryTree* tree, const std::string& word) {
+    if (tree->root == nullptr) {
+        return;
+    };
 
+    SearchResult result = {0, {}, 0.0, 0};
+    auto start = system_clock::now();
+
+    Node* current = tree->root;
+    while (current->word != word) {
+        result.numComparisons ++;
+        if (current->word == word) {
+            result.found = 1;
+            result.documentIds = current->documentIds;
+        } else if (current->word < word) {
+            current = current->right;
+        } else {
+            current = current->left;
+        }
+    };
+
+    auto stop = system_clock::now();
+    result.executionTime = duration<double>(stop - start).count();
+    return result;    
 };
 
 
