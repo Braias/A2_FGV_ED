1714a298c853aeeca464ade641d5f2b73089ed81
Braias <bryansm00001@gmail.com>
2025-06-02 22:34:40 -0300

    função read_doc implementada

--- src/data.cpp
+++ src/data.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-namespace dataHandling{
+namespace DataHandling{
     vector<string> read_directory(const string &dir_path){
         // Cria um vetor de strings para o nome dos arquivos
         vector<string> files;
@@ -37,5 +37,54 @@ namespace dataHandling{
         }
         return content;
     };
+
+    Document read_doc(const string &doc_path, int document_id, string conteudo){
+        // Extrai o nome do arquivo do caminho completo
+        size_t pos = doc_path.find_last_of("/\\");
+        string file_name = (pos == string::npos) ? doc_path : doc_path.substr(pos + 1);
+
+        Document doc;
+        doc.id = document_id;
+        doc.name = file_name;
+
+        // Tokenização do conteúdo
+        vector<string> tokens;
+        size_t start = 0;
+        size_t end = 0;
+        
+        while (start < conteudo.length()) {
+            // Encontra próximo espaço
+            end = conteudo.find(' ', start);
+            
+            // Extrai token
+            string token = conteudo.substr(start, end - start);
+            if (!token.empty()) {
+                tokens.push_back(token);
+            }
+            
+            // Fim da string
+            if (end == string::npos) break;
+            
+            // Avança para próximo token
+            start = end + 1;
+        };
+
+        vector<string> unique_tokens;
+        for (const string& token : tokens) {
+            bool exists = false;
+            for (const string& unique : unique_tokens) {
+                if (unique == token) {
+                    exists = true;
+                    break;
+                }
+            }
+            if (!exists) {
+                unique_tokens.push_back(token);
+            }
+        }
+        doc.content = unique_tokens;
+        return doc;
+    }
+
 
 }
