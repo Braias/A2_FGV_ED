924c9cc8009f39d839374c7d26d23d357f957965
Nicholas2104 <nicholasnogueira.costa@gmail.com>
2025-06-04 10:19:47 -0300

    verificação de read_directory

--- src/main_bst.cpp
+++ src/main_bst.cpp
@@ -1,3 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "bst.h"
+#include "data.h"
+
 int main(){
-    return 0;
+    string path;
+    std::cout<<"Insere caminho para documentos a serem processados: ";
+    std::cin >> path;
+    vector<string> file_paths = DataHandling::read_directory(path);
+    std::cout << "Directory is empty (1/0): " << file_paths.empty();
 }
