bfea71317348a372a338bc146a4c84de36701733
Braias <bryansm00001@gmail.com>
2025-06-09 07:50:30 -0300

    implementação AVL search

--- src/avl.cpp
+++ src/avl.cpp
@@ -15,5 +15,36 @@ namespace AVL {
         return tree;
     }
 
-    
+    SearchResult search(BinaryTree* tree, const std::string& word) {
+        SearchResult result = {0, {}, 0.0, 0};
+        
+        // Verifica a existência da árvore
+        if (tree->root == nullptr) {
+            return result;
+        };
+
+        // Começa o cronômetro
+        auto start = system_clock::now();
+
+        Node* current = tree->root;
+        // Loop de comparação
+        do {
+            result.numComparisons ++;
+            if (current->word == word) {
+                result.found = 1;
+                result.documentIds = current->documentIds;
+                break;
+            } else if (current->word < word) {
+                current = current->right;
+            } else {
+                current = current->left;
+            }
+        }while (current != nullptr);
+
+        // Para o relógio
+        auto stop = system_clock::now();
+        result.executionTime = duration<double>(stop - start).count();
+        return result;    
+    };
+
 }
