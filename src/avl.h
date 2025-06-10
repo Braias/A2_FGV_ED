00f3ca7bf5a89cc90191b69dd4aadff7a305f836
Braias <bryansm00001@gmail.com>
2025-06-10 14:22:24 -0300

    Documentação avl.h

--- src/avl.h
+++ src/avl.h
@@ -16,10 +16,31 @@ namespace AVL {
      */
     InsertResult insert(BinaryTree* tree, const std::string& word, int documentId);
 
+    /**
+     * @brief Calcula a altura de um nó numa árvore
+     * 
+     * @param node Ponteiro para o nó dentro da AVL 
+     * 
+     * @return Altura do nó na árvore balanceada
+     */
     int get_height(Node* node);
 
+    /**
+     * @brief Calcula o balanceamento da subárvore com raiz node
+     * Se balance é maior que 1, a árvore está desbalanceada para a direita
+     * Se balance é menor que 1, a árvore está desbalanceada para a esquerda
+     * 
+     * @param node Ponteiro para o nó a ser analisado
+     * 
+     * @return Número de balanceamento 
+     */
     int get_balance(Node* node);
 
+    /**
+     * @brief Calcula a nova altura de um nó (usada após a rotação)
+     * 
+     * @param node Ponteiro para o nó que terá a altura atualizada
+     */
     void new_height(Node* node);
 }
 
